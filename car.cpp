b982316ea2c01e018f073395f0a60af54e80e074
DESKTOP-LEJPUHO <shahneelaiqbal1@gmail.com>
2022-12-17 21:50:49 +0500

    code

---
+++ car.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+main(){
+
+          
+cout << "            ----------                 " << endl;
+cout << "           /          \\                " << endl;
+cout << "      ____/____________\\____           " << endl;
+cout << "     /                      \\          " << endl;
+cout << "    /                        \\         " << endl;
+cout << "   /                          \\        " << endl;
+cout << "  /   ....              ....   '''\\    " << endl;
+cout << " |___......____________......______|   " << endl;
+cout << "     ......            ......           " << endl;
+cout << "      ....              ....            " << endl;
+
+
+
+
+
+
+
+
+}
---
+++ headercolor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+main(){
+system("Color 97");
+cout << "-------------------------" << endl;
+cout << "-                       -" << endl;
+cout << "-   Shahneela's         -" << endl;
+cout << "-           Bakery      -" << endl; 
+cout << "-                       -" << endl;
+cout << "-                       -" << endl;
+cout << "-------------------------";                       
+
+
+}
